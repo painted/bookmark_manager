efb4fa8151514e0c3e10a66377c63906eba784b4
Painted <will.allen@paintedltd.co.uk>
2014-06-17 12:06:57 +0100

    added ability to filter by tag

--- lib/server.rb
+++ lib/server.rb
@@ -22,9 +22,13 @@ end
 post '/links' do
   url = params["url"]
   title = params["title"]
-  tags = params["tags"].split(" ").map do |tag|
-  	Tag.first_or_create(:text => tag)
-  end
+  tags = params["tags"].split(" ").map{|tag| Tag.first_or_create(:text => tag)}
   Link.create(:url => url, :title => title, :tags => tags)
   redirect to('/')
+end
+
+get '/tags/:text' do
+	tag = Tag.first(:text => params[:text])
+	@links = tag ? tag.links : []
+	erb :index
 end
