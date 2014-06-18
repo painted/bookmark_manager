658d10782f1e7e7782991c63ab331704b86cbebe
Painted <will.allen@paintedltd.co.uk>
2014-06-18 16:32:06 +0100

    add user accounts ability to sign out

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -28,3 +28,27 @@ feature "User signs in" do
 	end
 
 end
+
+feature 'User signs out' do 
+
+	before(:each) do 
+		User.create(:email => "test@test.com",
+					:password => 'test',
+					:password_confirmation => 'test')
+	end
+
+	scenario 'while being signed in' do 
+		sign_in('test@test.com', 'test')
+		click_button "Sign Out"
+		expect(page).to have_content("Good bye!")
+		expect(page).not_to have_content("Welcome, test@test.com")
+	end
+
+	def sign_in(email, password)
+		visit '/sessions/new'
+		fill_in 'email', :with => email
+		fill_in 'password', :with => password
+		click_button 'Sign In'
+	end
+
+end
